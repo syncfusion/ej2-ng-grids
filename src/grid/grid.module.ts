638cbf1486c20af1f1084d24dbb0781ab9002a0b
pipeline <pipeline@syncfusion.com>
2017-11-08 11:47:21 +0000

    Committing to angular development dependencies

--- src/grid/grid.module.ts
+++ src/grid/grid.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ColumnDirective, ColumnsDirective } from './columns.directive';
 import { AggregateColumnDirective, AggregateColumnsDirective } from './aggregate-columns.directive';
 import { AggregateDirective, AggregatesDirective } from './aggregates.directive';
-import { GridComponent, FilterService, PageService, SelectionService, SortService, GroupService, ReorderService, RowDDService, DetailRowService, ToolbarService, AggregateService, SearchService, VirtualScrollService, EditService, ResizeService, ExcelExportService, PdfExportService, CommandColumnService, ContextMenuService, ColumnMenuService } from './grid.component';
+import { GridComponent, FilterService, PageService, SelectionService, SortService, GroupService, ReorderService, RowDDService, DetailRowService, ToolbarService, AggregateService, SearchService, VirtualScrollService, EditService, ResizeService, ExcelExportService, PdfExportService, CommandColumnService, ContextMenuService, FreezeService, ColumnMenuService } from './grid.component';
 
 /**
  * NgModule definition for the Grid component.
